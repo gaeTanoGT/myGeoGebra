c17d8c50db1701eb0d7287b436bcc34ed2e7a178
TanogT <gaetanotorrisi18@gmail.com>
2023-05-16 17:34:59 +0200

    0.2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//const math = require("mathjs");
+let n;
+let funzione;
+let k = 10000;     //indice di precisione
 
 function pulisci(){
     console.log("ho pulito");
@@ -7,39 +11,83 @@ function pulisci(){
 }
 
 const inner = document.getElementById("bottone").onclick = function(){
-    let funzione = document.getElementById("inputField").value;
+    funzione = document.getElementById("inputField").value;
+
+    disegnaFunzione();
     
     let x1 = document.getElementById("x1").value;
     let x2 = document.getElementById("x2").value;
     
     console.log(`x11: ${x1}  --  x22: ${x2}`);
 
-    let bX;
-    let bY;
+    n = Math.log2(Math.abs(x1 - x2) * 500 * k);
+    console.log("n: ", n);
+    
+    let soluzione = trovaSoluzione(x1, x2)
+    console.log(soluzione);
+    document.getElementById("p1").innerHTML = `x: ${soluzione}`;
 
-    let st = 100;
-    while(st > 0){
-        bX = Math.abs(((x2 - x1) / 2)) + Number(x1);
-        
-        let strFunzione = funzione.replaceAll("x", bX);
+}
 
-        bY = math.eval(strFunzione);
-        console.log(`x: ${bX}  --  y: ${bY}`);
+function trovaSoluzione(ax, bx){
+    n--;
 
-        if(bY > 0){
-            x2 = bX;
-        }else if(bY < 0){
-            x1 = bX;
-        }else{
-            st = false;
-            document.getElementById("p1").innerHTML = `x: ${bX} - y: ${bY}`;
-            //bx - by sono le soluzioni
-        }
+    let strFunzione = funzione.replaceAll("x", ax);
+    let ay = math.eval(strFunzione);
 
-        st--;
+    strFunzione = funzione.replaceAll("x", bx);
+    let by = math.eval(strFunzione);
+
+    if(ay * by > 0){
+        console.log("Errore");
+        return null;
     }
 
-    document.getElementById("p1").innerHTML = `x: ${bX} - y: ${bY}`;
+    let cx;
+    if(ax < bx){
+        cx = (Math.abs(ax - bx) / 2) + Number(ax);
+    }
+    else{
+        cx = (Math.abs(ax - bx) / 2) + Number(bx);
+    }
 
-    console.log("finito");
+    console.log("cx: ", cx);
+    
+    strFunzione = funzione.replaceAll("x", cx);
+    let cy = math.eval(strFunzione);
+
+    if(((cy > 0 && by < 0) || (cy < 0 && by > 0)) && n > 0){
+        return trovaSoluzione(cx, bx);
+    } else if(((cy > 0 && by > 0) || (cy < 0 && by < 0)) && n > 0){
+        return trovaSoluzione(ax, cx);
+    } else {
+        return cx;
+    }
+}
+
+function disegnaFunzione(){
+    let z = 20;  //zoom canvas
+
+    var tag = document.getElementById("mioCanvas");
+    contesto = tag.getContext("2d");
+
+    contesto.clearRect(0, 0, 400, 400);
+
+    contesto.beginPath();
+    contesto.moveTo(0, 200);
+    contesto.lineTo(400, 200);
+    contesto.moveTo(200, 0);
+    contesto.lineTo(200, 400);
+    contesto.strokeStyle = "#9e9190";
+    contesto.stroke();
+
+    for(let i = -10; i < 10; i += 0.01){
+        let strFunzione = funzione.replaceAll("x", i);
+        let y = math.eval(strFunzione);
+
+        contesto.beginPath();
+        contesto.strokeStyle = "#000000";
+        contesto.fillRect((i) * z + 200, (-y) * z + 200, 0.5, 0.5);
+        contesto.stroke();
+    }
 }
